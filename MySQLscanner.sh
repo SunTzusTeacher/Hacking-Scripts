0c77be442279f8c884ec3fffb631af6d763e8d9d
Samhain <108845886+SunTzusTeacher@users.noreply.github.com>
2023-11-20 13:08:20 -0500

    Update MySQLscanner.sh

--- MySQLscanner.sh
+++ MySQLscanner.sh
@@ -1,6 +1,20 @@
 #! /bin/bash
 
-# This script is designed to find hosts with MySQL installed. On a LAN.
-nmap -ST 192.168.181.0/24 -p 3306 >/ddev/null -oG MySQLscan
+# This script is designed to find hosts with MySQL installed.
+
+# Prompt and read first IP
+echo "Enter starting IP adress : "
+read FirstIP
+
+# Prompt and read last octet of the last IP
+echo "Enter the last octet of the last IP adress : "
+read LastOctetIP
+
+# Prompt and readd the port you want to scan
+echo "Enter the port numbbber you want to scan for : "
+read port
+
+
+nmap -ST $FirstIP-$LastOctetIP -p $port >/dev/null -oG MySQLscan
 cat MySQLscan | grep open > MySQLscan2
 cat MySQLscan2
